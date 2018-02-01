f638073c2cf343850b2984cc6988f005d98e7a49
zhycn <qzhservice@gmail.com>
2018-02-01 16:18:34 +0800

    Add xbird-starter-retrofit2 module.

---
+++ xbird-starter-retrofit2/src/main/java/com/github/zhycn/retrofit2/Retrofit2AutoConfiguration.java
@@ -0,0 +1,10 @@
+package com.github.zhycn.retrofit2;
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigurationProperties(Retrofit2Properties.class)
+public class Retrofit2AutoConfiguration {
+
+}
