8b4444d9a3ec046f1e87de26fe8c5232f80dca97
zhycn <qzhservice@gmail.com>
2018-04-12 17:59:54 +0800

    Initial dubbo and resteasy commit.

---
+++ xbird-starter-dubbo-resteasy/src/main/java/com/github/zhycn/dubbo/resteasy/autoconfigure/DubboRestEasyAutoConfiguration.java
@@ -0,0 +1,14 @@
+package com.github.zhycn.dubbo.resteasy.autoconfigure;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Dubbo RestEasy Auto-configuration
+ * 
+ * @author zhycn
+ * @since 2.0.0 2018-01-30
+ */
+@Configuration
+public class DubboRestEasyAutoConfiguration {
+
+}
---
+++ xbird-starter-dubbo/src/main/java/com/github/zhycn/dubbo/autoconfigure/DubboAutoConfiguration.java
@@ -0,0 +1,14 @@
+package com.github.zhycn.dubbo.autoconfigure;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Dubbo Auto-configuration
+ * 
+ * @author zhycn
+ * @since 2.0.0 2018-04-12
+ */
+@Configuration
+public class DubboAutoConfiguration {
+
+}
